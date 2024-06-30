6c6f2dc87ee499e78a61d78673a0c4165fdffe4a
Clark Miller <clarkmillermail@gmail.com>
2024-06-30 17:33:52 -0400

    Almost finished the side scrolling rendering

--- main.cpp
+++ main.cpp
@@ -3,163 +3,103 @@
 #include <map>
 #include <chrono>
 #include <thread>
+#include <random>
 
 using namespace std;
 
-typedef vector<vector<bool>> twoDVec;
+typedef vector<vector<int>> twoDVec;
+typedef pair<int, int> coord;
 
-class FullScreen{
-    public:
-        unsigned int const WIDTH = 80;
-        unsigned int const HEIGHT = 16;
-        
-        //2D bool vector typedef
-        
-        map<string, twoDVec> objsMap;
+bool isObstacle(twoDVec& game, coord coords){
+    return game[coords.first][coords.second] == 2;
+}
 
-        bool screenMap[16][80];
-    public:
-        FullScreen(){
-            zeroOut();
-        }   
-
-        void zeroOut(){
-            for(unsigned int h = 0; h < HEIGHT; h++){
-                for(unsigned int w = 0; w < WIDTH; w++){
-                    screenMap[h][w] = 0;
-                }
-            }
-        }
-
-        void print(){
-            for(unsigned int h = 0; h < HEIGHT; h++){
-                for(unsigned int w = 0; w < WIDTH; w++){
-                    const char output = (screenMap[h][w]) ? '#' : ' ';
-                    cout << output;
-                }
-                //Spacing
-                cout << endl;
-            }
-        }
-        
-        void addToMap(unsigned int x, unsigned int y, const bool customChar[8][5]){
-            //Calc region to draw to
-            if(x >= 0 && x < 80 && y >= 0 && y <= 16){
-                unsigned int charx = 0, chary = 0;
-                for(unsigned int h = y; h < 8; h++){
-                    charx = 0;
-                    for(unsigned int w = x; w < 5; w++){
-                        screenMap[h][w] = customChar[chary][charx];
-                        charx++;
-                    }
-                    chary++;
-                }
-            }
-        }
-
-        void printChar(const bool customChar[8][5] ){
-            //Outer loop to iterate rows
-            for(size_t i = 0; i < 8; i++){
-                //Inner loop to print columns
-                for(size_t j = 0; j < 5; j++){
-                    const char output = (customChar[i][j]) ? '#' : ' ';
-                    cout << output;
-                }
-                cout << endl;
-            }
-        }
-};
-
-void printChar(const twoDVec& customChar){
-    //Outer loop to iterate rows
-    for(size_t i = 0; i < 8; i++){
-        //Inner loop to print columns
-        for(size_t j = 0; j < 5; j++){
-            const char output = (customChar[i][j]) ? '#' : 'O';
-            cout << output;
+void printGame(twoDVec& game){
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 32; j++){
+            const char cChar = (game[i][j] == 1 || game[i][j] == 2) ? '#' : 'O';
+            cout << cChar;
         }
         cout << endl;
     }
 }
 
-//Combines each 8 x 5 region into a full bitmapped screen
-void zeroOutMap(map<pair<int, int>, twoDVec> &screenMap){
-    const twoDVec blankChar = {
-        {0,0,0,0,0},
-        {0,0,0,0,0},
-        {0,0,0,0,0},
-        {0,0,0,0,0},
-        {0,0,0,0,0},
-        {0,0,0,0,0},
-        {0,0,0,0,0},
-        {0,0,0,0,0}
-    };
-
-
-    pair<int, int> it;
-    for(int i = 0; i < 2; i++){
-        for(int j = 0; j < 16; j++){
-            it = {i,j};
-            screenMap[it] = blankChar;
-        }
-    }
+void draw(twoDVec& game, const coord& coords, int objType){
+    game[coords.first][coords.second] = objType;
 }
 
-void printMap(map<pair<int, int>, twoDVec> &screenMap){
-    pair<int, int> it;
+pair<int, int> spawnObstacle(twoDVec& game, std::mt19937& gen){
+    int ran = 0;
+
+    std::uniform_int_distribution<> dis(16, 31);
+    do{
+        ran = dis(gen);
+        coord coords{1, ran};
+        const bool coordOccupied = isObstacle(game, coords); 
+
+        //Check if point where you're spawning obstacle already has something in it
+
+        if(coordOccupied){
+            continue;
+        }
+        if(isObstacle(game, coord(1, ran - 1)) || isObstacle(game, coord(1, ran + 1))){
+            continue;
+        }
+
+        return coords;
+    }while(true);
+}
+
+void shiftScreen(twoDVec& game){
     for(int i = 0; i < 2; i++){
-        for(int j = 0; j < 16; j++){
-            it = {i,j};
-            printChar(screenMap[it]);
-            //screenMap[it] = blankChar;
-            std::thread t([](){
-            std::this_thread::sleep_for(std::chrono::milliseconds(500));
-            system("clear");  
-            });
-            t.join();  // Wait for the thread to finish
+        for(int j = 0; j < 32; j++){
+            if(game[i][j] && j - 1 >= 0){
+                game[i][j] = 0;
+                game[i][j - 1] = 1;
+            }
         }
     }
 }
 
 int main(){
-    // bool aChar[8][5] = {
-    //     {1,1,1,1,1},
-    //     {1,0,0,0,1},
-    //     {1,0,0,0,1},
-    //     {1,1,1,1,1},
-    //     {1,0,0,0,1},
-    //     {1,0,0,0,1},
-    //     {1,0,0,0,1},
-    //     {1,0,0,0,1}
-    // };
+    std::random_device rd;  // Obtain a random seed from hardware
+    std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
 
-    //printChar(aChar);
-    // FullScreen screen;
-    // screen.addToMap(0, 0, aChar);
-   
-    // const twoDVec dino = {
-    //     {1,1},
-    //     {1,1},
-    // };
+    pair<int, int> playerCoords(1, 0);
 
-    // screen.objsMap["dino"] = dino;
-
-    //screen.print();
-
-    map<pair<int, int>, twoDVec> screenMap;
-    zeroOutMap(screenMap);
-    
-    twoDVec cus = {
-        {1,1,1,1,1},
-        {1,0,0,0,1},
-        {1,1,1,1,1},
-        {1,0,0,0,1},
-        {1,0,0,0,1},
-        {1,0,0,0,1},
-        {1,0,0,0,1},
-        {1,0,0,0,1},
+    twoDVec game = {
+        // 2 x 32 game screen. Half is used strictly for spawning in the obstacles off-screen
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
     };
-    screenMap[pair<int,int> {0, 0}] = cus;
 
-    printMap(screenMap);
+    draw(game, playerCoords, 1);
+
+    while(true){
+        // char input;
+        // cin >> std::noskipws >> input;
+
+        // cin.clear();
+        //cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        //addCoords(game, playerCoords, 0);
+
+        //playerCoords = {0, 0};
+        //addCoords(game, playerCoords, 1);
+        //printGame(game);
+        //addCoords(game, playerCoords, 0);
+
+        std::thread t([](){
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            system("clear");  
+        });
+        t.join();  // Wait for the thread to finish
+
+        //playerCoords = {1, 0};
+        //addCoords(game, playerCoords, 1);
+        draw(game, spawnObstacle(game, gen), 1);
+
+        printGame(game);
+        shiftScreen(game);
+    }
 }
